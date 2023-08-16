d5fe0b9161dbb20a453803a9cda17186f8a17a44
RevancifyX <140808499+RevancifyX@users.noreply.github.com>
2023-08-16 21:13:44 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -9,12 +9,12 @@ async def viewthumb(client, message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("**Sorry, No thumbnail found ❗**") 
+        await message.reply_text("__**Sorry, No thumbnail found ❗**__") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("__**Your Thumbnail deleted **__✅️")
+    await message.reply_text("__**Thumbnail deleted **__✅️")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
